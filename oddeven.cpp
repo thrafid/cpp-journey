ee0b6b941367cf48d22f782ae825864a9ec3b9c2
thrafid <rafidofficial02@gmail.com>
2024-08-31 18:07:45 +0600

    Create oddeven.cpp

---
+++ oddeven.cpp
@@ -0,0 +1,15 @@
+//Exercise 2: Odd or Even Checker
+#include <iostream>
+
+int main() {
+    int a;
+    std::cin >> a;
+    if(a%2 ==0){
+        std::cout << "the number is even!!";
+    }
+    else{
+        std::cout << "the number is odd!!";
+    }
+    return 0;
+}
+    
